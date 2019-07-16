7ce9059311f9e2b25be0fbba1ebbfd267761de98
Maksim <vad831s56@rambler.ru>
2019-07-16 23:24:43 +0300

    Проверка и валидация добавляемых данных в форме

--- src/main/java/ru/example/max/controller/MainController.java
+++ src/main/java/ru/example/max/controller/MainController.java
@@ -2,9 +2,11 @@ package ru.example.max.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.example.max.model.User;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,12 +34,16 @@ public class MainController {
     }
 
     @GetMapping("/users/new")
-    public String getSignUp() {
-        return "sign_up";
+    public String getSignUp(Model model) {
+        model.addAttribute("user", new User());
+        return "/sign_up";
     }
 
     @PostMapping("/users/new")
-    public String signUp(@ModelAttribute User user) {
+    public String signUp(@ModelAttribute @Valid User user, BindingResult result) {
+        if(result.hasErrors()) {
+            return "/sign_up";
+        }
         users.add(user);
         return  "redirect:/users";
     }
